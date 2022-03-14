3cf262ec77f195fdff315b91064fa09c64497032
PIYUSH DANGI <80493972+ignad01@users.noreply.github.com>
2022-03-14 22:27:46 +0530

    Add files via upload

---
+++ srchScript.js
@@ -0,0 +1,8 @@
+const search = document.getElementById('ser_search');
+const submit = document.getElementById('ser_submit');
+const change = document.getElementById("a-ser-btn");
+const serBtn = document.getElementById("ser-btn");
+const serFlex = document.getElementById("ser-flex");
+
+
+
